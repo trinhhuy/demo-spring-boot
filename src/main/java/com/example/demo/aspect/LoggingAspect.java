44309bf00934bbe3a0d51a1e200a2d268ff6c78c
trinhhuy <trinhhuyk57@gmail.com>
2025-03-13 17:01:53 +0700

    chore: betiauful message

--- src/main/java/com/example/demo/aspect/LoggingAspect.java
+++ src/main/java/com/example/demo/aspect/LoggingAspect.java
@@ -19,28 +19,54 @@ public class LoggingAspect {
     }
 
     @Around("execution(* com.example.demo.controllers..*.*(..))")
-    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+    public Object logAroundController(ProceedingJoinPoint joinPoint) throws Throwable {
         try {
             String className = joinPoint.getSignature().getDeclaringTypeName();
             String methodName = joinPoint.getSignature().getName();
 
             // Thêm controller và method name vào MDC
-            MDC.put("controller", className);
+            MDC.put("className", className);
             MDC.put("function", methodName);
 
             // Log request
             Object[] args = joinPoint.getArgs();
-            loggingService.log("Start: {}.{}", className, methodName, args);
+            loggingService.log("Start: ", args);
 
             Object result = joinPoint.proceed();
 
             // Log response cho các phương thức đồng bộ
             String responseData = result != null ? objectMapper.writeValueAsString(result) : "void";
-            loggingService.log("Finish: {}.{}", className, methodName, responseData);
+            loggingService.log("Finish: ", responseData);
             
             return result;
         } finally {
             MDC.clear();
         }
     }
+
+//    @Around("execution(* com.example.demo.services..*.*(..))")
+//    public Object logAroundService(ProceedingJoinPoint joinPoint) throws Throwable {
+//        try {
+//            String className = joinPoint.getSignature().getDeclaringTypeName();
+//            String methodName = joinPoint.getSignature().getName();
+//
+//            // Thêm service và method name vào MDC
+//            MDC.put("className", className);
+//            MDC.put("function", methodName);
+//
+//            // Log request
+//            Object[] args = joinPoint.getArgs();
+//            loggingService.log("Start Service: {}.{}", className, methodName, args);
+//
+//            Object result = joinPoint.proceed();
+//
+//            // Log response cho các phương thức đồng bộ
+//            String responseData = result != null ? objectMapper.writeValueAsString(result) : "void";
+//            loggingService.log("Finish Service: {}.{}", className, methodName, responseData);
+//
+//            return result;
+//        } finally {
+//            MDC.clear();
+//        }
+//    }
 }
